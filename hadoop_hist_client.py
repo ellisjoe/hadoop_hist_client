cc32b02243575bf0daa02edd208db7772542f178
Joe Ellis <ellis125@gmail.com>
2015-05-06 03:56:22 -0500

    add caching to all classes

--- hadoop_hist_client.py
+++ hadoop_hist_client.py
@@ -13,18 +13,22 @@ class JobHistoryServer:
     def __init__(self, host, port=19888):
         self.host = host
         self.port = port
+        self.jobs = None
         self.url = "http://" + host + ":" + str(port) + "/ws/v1/history/mapreduce"
 
     def request(self, uri):
         return requests.get(self.url + uri).json()
 
     def jobs(self):
-        return Jobs(self)
+        if self.jobs == None:
+            self.jobs = Jobs(self)
+        return self.jobs
 
 class Jobs:
 
     def __init__(self, jobHistoryServer):
         self.server = jobHistoryServer
+        self.jobs = None
         self.update()
 
     def __str__(self):
@@ -41,10 +45,17 @@ class Jobs:
         self.jobs = self.raw['jobs']['job']
 
     def all(self):
-        return [self.to_job(job) for job in self.jobs]
+        if self.jobs == None:
+            self.jobs = [self.to_job(job) for job in self.jobs]
+        return self.jobs
 
     def job_ids(self):
         return [job['id'] for job in self.jobs]
+
+    def find_by(self, key, value):
+        for job in self.jobs:
+            if job[key] == value:
+                return self.to_job(job)
 
     def filter(self, key, value):
         return [self.to_job(job) for job in self.jobs if job[key] == value]
@@ -60,6 +71,8 @@ class Job:
     def __init__(self, jobHistoryServer, job_id):
         self.server = jobHistoryServer
         self.job_id = job_id
+        self.tasks_list = None
+        self.counters = None
         self.update()
 
     def __getitem__(self, key):
@@ -79,12 +92,16 @@ class Job:
         return '/jobs/' + self.job_id
 
     def tasks(self):
-        tasks = self.server.request(self.uri() + '/tasks')['tasks']['task']
-        return [Task(self.server, self.job_id, task['id']) for task in tasks]
+        if self.tasks_list == None:
+            tasks = self.server.request(self.uri() + '/tasks')['tasks']['task']
+            self.tasks_list = [Task(self.server, self.job_id, task['id']) for task in tasks]
+        return self.tasks_list
 
     def counters(self):
-        counters = self.server.request(self.uri() + '/counters')
-        return Counters(counters)
+        if self.counters == None:
+            counters = self.server.request(self.uri() + '/counters')
+            self.counters = Counters(counters)
+        return self.counters
 
 class Task:
 
@@ -92,6 +109,8 @@ class Task:
         self.server = jobHistoryServer
         self.job_id = job_id
         self.task_id = task_id
+        self.attempts_list = None
+        self.counters = None
         self.update()
 
     def __str__(self):
@@ -111,12 +130,16 @@ class Task:
         return '/jobs/' + self.job_id + '/tasks/' + self.task_id
 
     def counters(self):
-        counters = self.server.request(self.uri() + '/counters')
-        return Counters(counters)
+        if self.counters == None:
+            counters = self.server.request(self.uri() + '/counters')
+            self.counters = Counters(counters)
+        return self.counters
 
     def attempts(self):
-        attempts = self.server.request(self.uri() + '/attempts')['taskAttempts']['taskAttempt']
-        return [Attempt(self.server, self.job_id, self.task_id, attempt['id']) for attempt in attempts]
+        if self.attempts_list == None:
+            attempts = self.server.request(self.uri() + '/attempts')['taskAttempts']['taskAttempt']
+            self.attempts_list = [Attempt(self.server, self.job_id, self.task_id, attempt['id']) for attempt in attempts]
+        return self.attempts_list
 
 class Attempt:
 
@@ -125,6 +148,7 @@ class Attempt:
         self.job_id = job_id
         self.task_id = task_id
         self.attempt_id = attempt_id
+        self.counters = None
         self.update()
 
     def __str__(self):
@@ -132,6 +156,9 @@ class Attempt:
 
     def __repr__(self):
         return self.__str__()
+
+    def __getitem__(self, key):
+        return self.attempt[key]
 
     def update(self):
         self.raw = self.server.request(self.uri())
@@ -141,8 +168,10 @@ class Attempt:
         return '/jobs/' + self.job_id + '/tasks/' + self.task_id + '/attempts/' + self.attempt_id
 
     def counters(self):
-        counters = self.server.request(self.uri() + '/counters')
-        return Counters(counters)
+        if self.counters == None:
+            counters = self.server.request(self.uri() + '/counters')
+            self.counters = Counters(counters)
+        return self.counters
 
 class Counters:
 
